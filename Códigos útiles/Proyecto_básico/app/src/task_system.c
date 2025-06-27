5912e23c27399fbee573f0d965971c77959cea1e
smabrera03 <santimcabrera@gmail.com>
2025-06-27 18:21:32 -0300

    primer commit

---
+++ Códigos útiles/Proyecto_básico/app/src/task_system.c
@@ -0,0 +1,187 @@
+/*
+ * Copyright (c) 2023 Juan Manuel Cruz <jcruz@fi.uba.ar> <jcruz@frba.utn.edu.ar>.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its
+ *    contributors may be used to endorse or promote products derived from
+ *    this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
+ * IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ *
+ * @file   : task_system.c
+ * @date   : Set 26, 2023
+ * @author : Juan Manuel Cruz <jcruz@fi.uba.ar> <jcruz@frba.utn.edu.ar>
+ * @version	v1.0.0
+ */
+
+/********************** inclusions *******************************************/
+/* Project includes. */
+#include "main.h"
+
+/* Demo includes. */
+#include "logger.h"
+#include "dwt.h"
+
+/* Application & Tasks includes. */
+#include "board.h"
+#include "app.h"
+#include "task_system_attribute.h"
+#include "task_system_interface.h"
+#include "task_actuator_attribute.h"
+#include "task_actuator_interface.h"
+
+/********************** macros and definitions *******************************/
+#define G_TASK_SYS_CNT_INI			0ul
+#define G_TASK_SYS_TICK_CNT_INI		0ul
+
+#define DEL_SYS_XX_MIN				0ul
+#define DEL_SYS_XX_MED				50ul
+#define DEL_SYS_XX_MAX				500ul
+
+/********************** internal data declaration ****************************/
+task_system_dta_t task_system_dta =
+	{DEL_SYS_XX_MIN, ST_SYS_STATE, EV_SYS_BTN_SUMA_PUSHED, false, 0};
+
+#define SYSTEM_DTA_QTY	(sizeof(task_system_dta)/sizeof(task_system_dta_t))
+
+/********************** internal functions declaration ***********************/
+
+/********************** internal data definition *****************************/
+const char *p_task_system 		= "Task System (System Statechart)";
+const char *p_task_system_ 		= "Non-Blocking & Update By Time Code";
+
+/********************** external data declaration ****************************/
+uint32_t g_task_system_cnt;
+volatile uint32_t g_task_system_tick_cnt;
+
+/********************** external functions definition ************************/
+void task_system_init(void *parameters)
+{
+	task_system_dta_t 	*p_task_system_dta;
+	task_system_st_t	state;
+	task_system_ev_t	event;
+	bool b_event;
+
+	/* Print out: Task Initialized */
+	LOGGER_LOG("  %s is running - %s\r\n", GET_NAME(task_system_init), p_task_system);
+	LOGGER_LOG("  %s is a %s\r\n", GET_NAME(task_system), p_task_system_);
+
+	g_task_system_cnt = G_TASK_SYS_CNT_INI;
+
+	/* Print out: Task execution counter */
+	LOGGER_LOG("   %s = %lu\r\n", GET_NAME(g_task_system_cnt), g_task_system_cnt);
+
+	init_queue_event_task_system();
+
+	/* Update Task Actuator Configuration & Data Pointer */
+	p_task_system_dta = &task_system_dta;
+
+	/* Print out: Task execution FSM */
+	state = p_task_system_dta->state;
+	LOGGER_LOG("   %s = %lu", GET_NAME(state), (uint32_t)state);
+
+	event = p_task_system_dta->event;
+	LOGGER_LOG("   %s = %lu", GET_NAME(event), (uint32_t)event);
+
+	b_event = p_task_system_dta->flag;
+	LOGGER_LOG("   %s = %s\r\n", GET_NAME(b_event), (b_event ? "true" : "false"));
+
+	g_task_system_tick_cnt = G_TASK_SYS_TICK_CNT_INI;
+}
+
+
+void prender_leds(uint8_t numero){ //con esta función mando la orden de prender las leds a los actuadores en función de el valor de numero
+	task_actuator_id_t id_list[] = {ID_LED1, ID_LED2, ID_LED4, ID_LED8};
+	int actuator_qty = sizeof(id_list)/sizeof(id_list[0]);
+	task_actuator_ev_t evento;
+	for(int i = 0; i < actuator_qty; i++){
+		evento = ((numero>>i) & 0x1) ? EV_LED_XX_ON : EV_LED_XX_OFF;
+		put_event_task_actuator(evento, id_list[i]);
+	}
+}
+
+
+void task_system_update(void *parameters)
+{
+	task_system_dta_t *p_task_system_dta;
+	bool b_time_update_required = false;
+
+	/* Update Task System Counter */
+	g_task_system_cnt++;
+
+	/* Protect shared resource (g_task_system_tick) */
+	__asm("CPSID i");	/* disable interrupts*/
+    if (G_TASK_SYS_TICK_CNT_INI < g_task_system_tick_cnt)
+    {
+    	g_task_system_tick_cnt--;
+    	b_time_update_required = true;
+    }
+    __asm("CPSIE i");	/* enable interrupts*/
+
+    while (b_time_update_required)
+    {
+		/* Protect shared resource (g_task_system_tick) */
+		__asm("CPSID i");	/* disable interrupts*/
+		if (G_TASK_SYS_TICK_CNT_INI < g_task_system_tick_cnt)
+		{
+			g_task_system_tick_cnt--;
+			b_time_update_required = true;
+		}
+		else
+		{
+			b_time_update_required = false;
+		}
+		__asm("CPSIE i");	/* enable interrupts*/
+
+    	/* Update Task System Data Pointer */
+		p_task_system_dta = &task_system_dta;
+
+		if (true == any_event_task_system())
+		{
+			p_task_system_dta->flag = true;
+			p_task_system_dta->event = get_event_task_system();
+		}
+		switch (p_task_system_dta->state){
+			case ST_SYS_STATE:
+				if(p_task_system_dta->flag == true){
+					if(p_task_system_dta->event == EV_SYS_BTN_SUMA_PUSHED){
+						p_task_system_dta->flag = false;
+						p_task_system_dta->numero = (p_task_system_dta->numero + 1)%16;
+						prender_leds(p_task_system_dta->numero);
+
+					}else if(p_task_system_dta->event == EV_SYS_BTN_RESTA_PUSHED){
+						p_task_system_dta->flag = false;
+						p_task_system_dta->numero = (p_task_system_dta->numero - 1 + 16)%16;
+						prender_leds(p_task_system_dta->numero);
+					}else{}
+				break;
+			default:
+				break;
+				}
+		}//MORALEJAS: MUY IMPORTANTES LOS BREAKES Y PONER LAS GUARDAS DEL FLAG
+	}
+}
+
+/********************** end of file ******************************************/
---
+++ Pruebas_workspace/.metadata/.plugins/org.eclipse.core.resources/.history/55/70739c74985300101fc0ec6dea96da34
@@ -0,0 +1,152 @@
+/*
+ * Copyright (c) 2023 Juan Manuel Cruz <jcruz@fi.uba.ar>.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its
+ *    contributors may be used to endorse or promote products derived from
+ *    this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
+ * IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * @file   : board.h
+ * @date   : Set 26, 2023
+ * @author : Juan Manuel Cruz <jcruz@fi.uba.ar> <jcruz@frba.utn.edu.ar>
+ * @version	v1.0.0
+ */
+
+#ifndef BOARD_INC_BOARD_H_
+#define BOARD_INC_BOARD_H_
+
+/********************** CPP guard ********************************************/
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/********************** inclusions *******************************************/
+
+/********************** macros ***********************************************/
+#define NUCLEO_F103RC		(0)
+#define NUCLEO_F401RE		(1)
+#define NUCLEO_F446RE		(2)
+#define NUCLEO_F429ZI		(3)
+#define NUCLEO_F439ZI		(4)
+#define NUCLEO_F413ZH		(5)
+#define STM32F429I_DISCO1	(6)
+
+#define BOARD (NUCLEO_F103RC)
+
+/* STM32 Nucleo Boards - 64 Pins */
+#if ((BOARD == NUCLEO_F103RC) || (BOARD == NUCLEO_F401RE) || (BOARD == NUCLEO_F446RE))
+
+#define BTN_A_PIN		B1_Pin
+#define BTN_A_PORT		B1_GPIO_Port
+#define BTN_A_PRESSED	GPIO_PIN_RESET
+#define BTN_A_HOVER		GPIO_PIN_SET
+
+//BTN SUMA
+#define BTN_SUMA_PIN		BTN_SUMA_Pin
+#define BTN_SUMA_PORT		BTN_SUMA_GPIO_Port
+#define BTN_SUMA_PRESSED	GPIO_PIN_SET
+#define BTN_SUMA_HOVER		GPIO_PIN_RESET
+
+//BTN RESTA
+#define BTN_RESTA_PIN		BTN_RESTA_Pin
+#define BTN_RESTA_PORT		BTN_RESTA_GPIO_Port
+#define BTN_RESTA_PRESSED	GPIO_PIN_SET
+#define BTN_RESTA_HOVER		GPIO_PIN_RESET
+
+//LED_A, led integrada a la placa
+#define LED_A_PIN		LD2_Pin
+#define LED_A_PORT		LD2_GPIO_Port
+#define LED_A_ON		GPIO_PIN_SET
+#define LED_A_OFF		GPIO_PIN_RESET
+
+//LED8
+#define LED8_PIN		LED8_Pin
+#define LED8_PORT		LED8_GPIO_Port
+#define LED8_ON		    GPIO_PIN_SET
+#define LED8_OFF		GPIO_PIN_RESET
+
+//LED4
+#define LED4_PIN		LED4_Pin
+#define LED4_PORT		LED4_GPIO_Port
+#define LED4_ON		    GPIO_PIN_SET
+#define LED4_OFF		GPIO_PIN_RESET
+
+//LED2
+#define LED2_PIN		LED2_Pin
+#define LED2_PORT		LED2_GPIO_Port
+#define LED2_ON		    GPIO_PIN_SET
+#define LED2_OFF		GPIO_PIN_RESET
+
+//LED1
+#define LED1_PIN		LED1_Pin
+#define LED1_PORT		LED1_GPIO_Port
+#define LED1_ON		    GPIO_PIN_SET
+#define LED1_OFF		GPIO_PIN_RESET
+#endif/* STM32 Nucleo Boards - 144 Pins */
+
+#if ((BOARD == NUCLEO_F429ZI) || (BOARD == NUCLEO_F439ZI) || (BOARD == NUCLEO_F413ZH))
+
+#define BTN_A_PIN		USER_Btn_Pin
+#define BTN_A_PORT		USER_Btn_GPIO_Port
+#define BTN_A_PRESSED	GPIO_PIN_SET
+#define BTN_A_HOVER		GPIO_PIN_RESET
+
+#define LED_A_PIN		LD1_Pin
+#define LED_A_PORT		LD1_GPIO_Port
+#define LED_A_ON		GPIO_PIN_SET
+#define LED_A_OFF		GPIO_PIN_RESET
+
+#endif
+
+/* STM32 Discovery Kits */
+#if (BOARD == STM32F429I_DISCO1)
+
+#define BTN_A_PIN		B1_Pin
+#define BTN_A_PORT		B1_GPIO_Port
+#define BTN_A_PRESSED	GPIO_PIN_SET
+#define BTN_A_HOVER		GPIO_PIN_RESET
+
+#define LED_A_PIN		LD3_Pin
+#define LED_A_PORT		LD3_GPIO_Port
+#define LED_A_ON		GPIO_PIN_SET
+#define LED_A_OFF		GPIO_PIN_RESET
+
+#endif
+
+/********************** typedef **********************************************/
+
+/********************** external data declaration ****************************/
+
+/********************** external functions declaration ***********************/
+
+/********************** End of CPP guard *************************************/
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* BOARD_INC_BOARD_H_ */
+
+/********************** end of file ******************************************/
