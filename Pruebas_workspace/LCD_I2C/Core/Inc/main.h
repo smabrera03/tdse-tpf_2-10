e7b466fc15adbb64aecb54c928e03553a9ae29b9
Lucas Arias Montes <correomultiusos24@gmail.com>
2025-07-30 19:04:25 -0300

    Actualizacion modulo display

--- Pruebas_workspace/LCD_I2C/Core/Inc/main.h
+++ Pruebas_workspace/LCD_I2C/Core/Inc/main.h
@@ -72,6 +72,10 @@ void Error_Handler(void);
 #define TCK_GPIO_Port GPIOA
 #define SWO_Pin GPIO_PIN_3
 #define SWO_GPIO_Port GPIOB
+#define BTN_RESTA_Pin GPIO_PIN_4
+#define BTN_RESTA_GPIO_Port GPIOB
+#define BTN_SUMA_Pin GPIO_PIN_5
+#define BTN_SUMA_GPIO_Port GPIOB
 
 /* USER CODE BEGIN Private defines */
 
---
+++ Pruebas_workspace/LCD_I2C/Core/Src/task_display.c
@@ -0,0 +1,498 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2025 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+#include "logger.h"
+
+#include "../../Librerias Tuto/I2C_LCD.h"
+#include "../../Librerias Tuto/I2C_LCD_cfg.h"
+#include "../../Librerias Tuto/Util.h"
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+#define MyI2C_LCD I2C_LCD_1
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+I2C_HandleTypeDef hi2c2;
+
+UART_HandleTypeDef huart2;
+
+/* USER CODE BEGIN PV */
+
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+static void MX_USART2_UART_Init(void);
+static void MX_I2C2_Init(void);
+/* USER CODE BEGIN PFP */
+
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+
+typedef enum task_display_st {ST_MOSTRAR_PUNTOS, ST_MENU_ENVIDO, ST_E, ST_RE, ST_FE} task_display_st_t;
+
+typedef enum task_display_ev {EV_DIS_BTN_ENV, EV_DIS_BTN_Q} task_display_ev_t;
+
+typedef struct
+{
+	uint32_t			tick;
+	task_display_st_t	state;
+	task_display_ev_t	event;
+} task_display_dta_t;
+
+// Defino los caracteres especiales para 1, 2, 3, 4 y 5 puntos
+uint8_t unPunto[] = {0x00, 0x00, 0x00, 0x10, 0x10, 0x10, 0x10, 0x10};
+uint8_t dosPuntos[] = {0x00, 0x00, 0x00, 0x1f, 0x10, 0x10, 0x10, 0x10};
+uint8_t tresPuntos[] = {0x00, 0x00, 0x00, 0x1f, 0x11, 0x11, 0x11, 0x11};
+uint8_t cuatroPuntos[] = {0x00, 0x00, 0x00, 0x1f, 0x11, 0x11, 0x11, 0x1f};
+uint8_t cincoPuntos[] = {0x00, 0x00, 0x00, 0x1f, 0x13, 0x15, 0x19, 0x1f};
+
+void mostrar_puntos(int puntos) {
+	int cant_de_cincos = puntos / 5;
+	int resto = puntos % 5;
+	int i = 0;
+	for (i; i<cant_de_cincos; i++) {
+		I2C_LCD_SetCursor(MyI2C_LCD, i, 0);
+		I2C_LCD_PrintCustomChar(MyI2C_LCD, 5);
+	}
+	if (resto != 0) {
+		I2C_LCD_SetCursor(MyI2C_LCD, i, 0);
+		I2C_LCD_PrintCustomChar(MyI2C_LCD, resto);
+	}
+}
+
+void task_display_init(void *parameters) {
+	uint32_t index;
+	task_sensor_dta_t *p_task_sensor_dta;
+	task_sensor_st_t state;
+	task_sensor_ev_t event;
+
+	/* Print out: Task Initialized */
+	LOGGER_INFO(" ");
+	LOGGER_INFO("  %s is running - %s", GET_NAME(task_sensor_init), p_task_sensor);
+	LOGGER_INFO("  %s is a %s", GET_NAME(task_sensor), p_task_sensor_);
+
+	/* Init & Print out: Task execution counter */
+	g_task_sensor_cnt = G_TASK_SEN_CNT_INIT;
+	LOGGER_INFO("   %s = %lu", GET_NAME(g_task_sensor_cnt), g_task_sensor_cnt);
+
+	for (index = 0; SENSOR_DTA_QTY > index; index++)
+	{
+		/* Update Task Sensor Data Pointer */
+		p_task_sensor_dta = &task_sensor_dta_list[index];
+
+		/* Init & Print out: Index & Task execution FSM */
+		state = ST_BTN_XX_UP;
+		p_task_sensor_dta->state = state;
+
+		event = EV_BTN_XX_UP;
+		p_task_sensor_dta->event = event;
+
+		LOGGER_INFO(" ");
+		LOGGER_INFO("   %s = %lu   %s = %lu   %s = %lu",
+					GET_NAME(index), index,
+					GET_NAME(state), (uint32_t)state,
+					GET_NAME(event), (uint32_t)event);
+	}
+}
+
+void task_display_update() {
+	bool b_time_update_required = false;
+
+	/* Protect shared resource */
+	__asm("CPSID i");	/* disable interrupts */
+	if (G_TASK_SEN_TICK_CNT_INI < g_task_sensor_tick_cnt)
+	{
+		/* Update Tick Counter */
+		g_task_sensor_tick_cnt--;
+		b_time_update_required = true;
+	}
+	__asm("CPSIE i");	/* enable interrupts */
+
+	while (b_time_update_required)
+	{
+		/* Update Task Counter */
+		g_task_sensor_cnt++;
+
+		/* Run Task Statechart */
+		task_sensor_statechart();
+
+		/* Protect shared resource */
+		__asm("CPSID i");	/* disable interrupts */
+		if (G_TASK_SEN_TICK_CNT_INI < g_task_sensor_tick_cnt)
+		{
+			/* Update Tick Counter */
+			g_task_sensor_tick_cnt--;
+			b_time_update_required = true;
+		}
+		else
+		{
+			b_time_update_required = false;
+		}
+		__asm("CPSIE i");	/* enable interrupts */
+	}
+}
+
+void task_display_statechart(void) {
+
+	switch (p_task_display_dta->state)
+	{
+		case ST_MOSTRAR_PUNTOS:
+
+			mostrar_puntos(puntos); // después veo de dónde viene el dato de los puntos
+
+			if (p_task_display_dta->event == EV_DIS_BTN_ENV) {
+
+				p_task_display_dta->state = ST_MENU_ENVIDO;
+			}
+
+			break;
+
+		case ST_MENU_ENVIDO:
+
+			//mostrar el menu
+
+			switch (p_task_display_dta->state)
+			{
+				case ST_E:
+
+					//seleccionar la casilla de envido
+
+					break;
+
+				case ST_RE:
+
+					//seleccionar la casilla de real envido
+
+					break;
+
+				case ST_FE:
+
+					//seleccionar la casilla de falta envido
+
+					break;
+			}
+
+	}
+
+}
+
+
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+
+  /* USER CODE BEGIN 1 */
+
+  /* USER CODE END 1 */
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_USART2_UART_Init();
+  MX_I2C2_Init();
+  /* USER CODE BEGIN 2 */
+
+  I2C_LCD_Init(MyI2C_LCD);
+
+  // Creo los caracteres especiales. A la función creadora hay que pasarle la instancia del LCD que se está usando,
+  // un índice que va a identificar a cada caracter y un array que representa los pixeles que se tienen que prender
+  // para mostrar el caracter
+  I2C_LCD_CreateCustomChar(MyI2C_LCD, 1, unPunto);
+  I2C_LCD_CreateCustomChar(MyI2C_LCD, 2, dosPuntos);
+  I2C_LCD_CreateCustomChar(MyI2C_LCD, 3, tresPuntos);
+  I2C_LCD_CreateCustomChar(MyI2C_LCD, 4, cuatroPuntos);
+  I2C_LCD_CreateCustomChar(MyI2C_LCD, 5, cincoPuntos);
+
+  int puntos = 0;
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+
+
+  while (1)
+  {
+	  I2C_LCD_Clear(MyI2C_LCD);
+	  I2C_LCD_SetCursor(MyI2C_LCD, 0, 0);
+
+	  /*
+	   * código para probar aumentar los puntos pulsando un botón, lo dejo por las dudas
+
+	  int puntos_actuales_idx = 0;
+  	  int col_actual = 0;
+
+	  if ( señal de suma ) {
+		  if (puntos_actuales_idx%5 == 0) {
+			  col_actual++;
+		  }
+		  puntos_actuales_idx = puntos_actuales_idx%5 + 1;
+	  } else if (señal de resta ) {
+		  if (puntos_actuales_idx == 1) {
+			col_actual--;
+			puntos_actuales_idx = 5;
+		  } else {
+			  puntos_actuales_idx--;
+		  }
+		  I2C_LCD_SetCursor(MyI2C_LCD, col_actual, fila_actual);
+	  }
+
+	  I2C_LCD_PrintCustomChar(MyI2C_LCD, puntos_actuales_idx);
+
+	  */
+
+	  mostrar_puntos((int)puntos);
+	  HAL_Delay(1000);
+	  puntos = (puntos+1)%30;
+	  I2C_LCD_Clear(MyI2C_LCD);
+	  I2C_LCD_SetCursor(MyI2C_LCD, 0, 0);
+
+    /* USER CODE END WHILE */
+
+    /* USER CODE BEGIN 3 */
+  }
+  /* USER CODE END 3 */
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI_DIV2;
+  RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL16;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/**
+  * @brief I2C2 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_I2C2_Init(void)
+{
+
+  /* USER CODE BEGIN I2C2_Init 0 */
+
+  /* USER CODE END I2C2_Init 0 */
+
+  /* USER CODE BEGIN I2C2_Init 1 */
+
+  /* USER CODE END I2C2_Init 1 */
+  hi2c2.Instance = I2C2;
+  hi2c2.Init.ClockSpeed = 100000;
+  hi2c2.Init.DutyCycle = I2C_DUTYCYCLE_2;
+  hi2c2.Init.OwnAddress1 = 0;
+  hi2c2.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
+  hi2c2.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
+  hi2c2.Init.OwnAddress2 = 0;
+  hi2c2.Init.GeneralCallMode = I2C_GENERALCALL_DISABLE;
+  hi2c2.Init.NoStretchMode = I2C_NOSTRETCH_DISABLE;
+  if (HAL_I2C_Init(&hi2c2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN I2C2_Init 2 */
+
+  /* USER CODE END I2C2_Init 2 */
+
+}
+
+/**
+  * @brief USART2 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_USART2_UART_Init(void)
+{
+
+  /* USER CODE BEGIN USART2_Init 0 */
+
+  /* USER CODE END USART2_Init 0 */
+
+  /* USER CODE BEGIN USART2_Init 1 */
+
+  /* USER CODE END USART2_Init 1 */
+  huart2.Instance = USART2;
+  huart2.Init.BaudRate = 115200;
+  huart2.Init.WordLength = UART_WORDLENGTH_8B;
+  huart2.Init.StopBits = UART_STOPBITS_1;
+  huart2.Init.Parity = UART_PARITY_NONE;
+  huart2.Init.Mode = UART_MODE_TX_RX;
+  huart2.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart2.Init.OverSampling = UART_OVERSAMPLING_16;
+  if (HAL_UART_Init(&huart2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN USART2_Init 2 */
+
+  /* USER CODE END USART2_Init 2 */
+
+}
+
+/**
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_GPIO_Init(void)
+{
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+/* USER CODE BEGIN MX_GPIO_Init_1 */
+/* USER CODE END MX_GPIO_Init_1 */
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOC_CLK_ENABLE();
+  __HAL_RCC_GPIOD_CLK_ENABLE();
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(LD2_GPIO_Port, LD2_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin : B1_Pin */
+  GPIO_InitStruct.Pin = B1_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING;
+  GPIO_InitStruct.Pull = GPIO_PULLDOWN;
+  HAL_GPIO_Init(B1_GPIO_Port, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : LD2_Pin */
+  GPIO_InitStruct.Pin = LD2_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(LD2_GPIO_Port, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : BTN_RESTA_Pin BTN_SUMA_Pin */
+  GPIO_InitStruct.Pin = BTN_RESTA_Pin|BTN_SUMA_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = GPIO_PULLUP;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+  /* EXTI interrupt init*/
+  HAL_NVIC_SetPriority(EXTI15_10_IRQn, 1, 0);
+  HAL_NVIC_EnableIRQ(EXTI15_10_IRQn);
+
+/* USER CODE BEGIN MX_GPIO_Init_2 */
+/* USER CODE END MX_GPIO_Init_2 */
+}
+
+/* USER CODE BEGIN 4 */
+
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
