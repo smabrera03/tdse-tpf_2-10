2795cf7dbd6d58852776b2492d052abb448b4404
smabrera03 <santimcabrera@gmail.com>
2025-08-07 12:09:23 -0300

    Cree la carpeta Workspace_final con la versión final del TP Pasé a PDF los archivos .rnote

---
+++ Workspace_final/Final/app/src/KEYPAD_cfg.c
@@ -0,0 +1,27 @@
+/*
+ * File: KEYPAD_cfg.c
+ * Driver Name: [[ 4x4 KEYPAD ]]
+ * SW Layer:   ECUAL
+ * Created on: Jun 28, 2020
+ * Author:     Khaled Magdy
+ * -------------------------------------------
+ * For More Information, Tutorials, etc.
+ * Visit Website: www.DeepBlueMbedded.com
+ *
+ */
+
+#include "KEYPAD.h"
+#include "main.h"
+
+const KEYPAD_CfgType KEYPAD_CfgParam[KEYPAD_UNITS] =
+{
+	// KeyPAD Unit 1 Configurations
+    {
+    	/* ROWs Pins Info */
+	    {R0_GPIO_Port, R1_GPIO_Port, R2_GPIO_Port, R3_GPIO_Port},
+		{R0_Pin, R1_Pin, R2_Pin, R3_Pin},
+		/* COLs Pins */
+		{C0_GPIO_Port, C1_GPIO_Port, C2_GPIO_Port, C3_GPIO_Port},
+		{C0_Pin, C1_Pin, C2_Pin, C3_Pin}
+	}
+};
